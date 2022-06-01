21b6edafb8fdaa966625c581de55afcbccd087cd
Ahaan25 <ahaan25092002@gmail.com>
2022-06-01 08:55:04 +0530

    Initial changes

--- app/src/main/java/com/example/omnitrixkotlin/SplashScreen.kt
+++ app/src/main/java/com/example/omnitrixkotlin/SplashScreen.kt
@@ -1,18 +1,20 @@
 package com.example.omnitrixkotlin
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import com.example.omnitrixkotlin.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : Activity() {
 
-    private lateinit var binding: ActivitySplashScreenBinding
-
+    private val SC:Long=1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, SC)
     }
 }
